5a99db4488ac5b0f6963bec0cb140a9d1e785c09
Ayoub <ayoubelleuch6@gmail.com>
2025-12-05 07:21:42 +0100

    test 4-2

--- src/de/luh/vss/chat/client/ChatClient.java
+++ src/de/luh/vss/chat/client/ChatClient.java
@@ -14,45 +14,46 @@ public class ChatClient {
             e.printStackTrace();
         }
     }
-
+    
     public void start() throws IOException {
 
-        //register for the lease
+        //Initialization
         var socket = new Socket("130.75.202.197", 4447);
         var writer = new DataOutputStream(new BufferedOutputStream(socket.getOutputStream()));
+        var reader = new DataInputStream(new BufferedInputStream(socket.getInputStream()));
         User.UserIdentifier MyUserIdentifier = new User.UserIdentifier(7567);
         
-        var request = new Message.ServiceRegistrationRequest(MyUserIdentifier,InetAddress.getByName("130.75.202.197"), 4447);
-        request.toStream(writer);
+        
+        
+        //The lease
+        Message.ServiceRegistrationRequest req =new Message.ServiceRegistrationRequest(MyUserIdentifier,socket.getInetAddress(),socket.getPort());
+        req.toStream(writer);
         writer.flush();
-        new Message.ChatMessagePayload(MyUserIdentifier, "TEST 4_1 RENEW LEASE").toStream(writer);
-        // keep the lease
-        var scheduler = Executors.newScheduledThreadPool(2);
-	        scheduler.scheduleWithFixedDelay(()->{
-	        	try {
-					request.toStream(writer);
-					writer.flush();
-					System.out.println("renewed lease");
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-	        	
-	        		}, 3,3,TimeUnit.MINUTES);
-	        
-	    scheduler.schedule(()->{
-	    	scheduler.shutdownNow();
-		    try {
-				writer.close();
-				socket.close();
-			} catch (IOException e) {
+        //trigger
+        new Message.ChatMessagePayload(MyUserIdentifier, "TEST 4_2 LISTEN TO INCOMING MESSAGES AND ECHO SPECIAL MESSAGE").toStream(writer);
+        writer.flush();
+        
+        
+        
+        Message recieved_msg = null ;
+        while(true) {
+			try {
+				recieved_msg = Message.parse(reader);
+			} catch (IOException | ReflectiveOperationException e) {
 				e.printStackTrace();
-			}},20,TimeUnit.MINUTES);
-	    
-	    try {
-			scheduler.awaitTermination(21,TimeUnit.MINUTES);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
+			}
+	    	if(  recieved_msg instanceof Message.ChatMessagePayload PayloadedMessage) {
+	    		String text =PayloadedMessage.getMessage();
+	    		if(text.contains("SPECIAL MESSAGE TEST 4_2")) {
+	    			recieved_msg.toStream(writer);
+	    			break;
+	    		}
+	    		
+	    	}
+	    }
+	    writer.close();
+	    reader.close();
+	    socket.close();
 
     }
 }
