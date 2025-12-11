e96ae251ab16deb38a3c2727b13b24d98e1052ab
Ayoub <ayoubelleuch6@gmail.com>
2025-12-11 09:14:18 +0100

    TEST 5_1

--- src/de/luh/vss/chat/client/ChatClient.java
+++ src/de/luh/vss/chat/client/ChatClient.java
@@ -5,7 +5,8 @@ import de.luh.vss.chat.common.*;
 
 import java.io.*;
 import java.net.*;
-import java.util.concurrent.*;
+import java.util.Arrays;
+import java.util.LinkedHashMap;
 public class ChatClient {
     public static void main(String... args) {
         try {
@@ -15,10 +16,16 @@ public class ChatClient {
         }
     }
     
+    
     public void start() throws IOException {
 
         //Initialization
-        var socket = new Socket("130.75.202.197", 4447);
+        var socket = new Socket("130.75.202.197", 4448);
+        var udpSocket = new DatagramSocket();
+        udpSocket.setSoTimeout(500);
+        ByteArrayOutputStream bout = new ByteArrayOutputStream();
+        boolean testLanched=false;
+        
         var writer = new DataOutputStream(new BufferedOutputStream(socket.getOutputStream()));
         var reader = new DataInputStream(new BufferedInputStream(socket.getInputStream()));
         User.UserIdentifier MyUserIdentifier = new User.UserIdentifier(7567);
@@ -30,30 +37,81 @@ public class ChatClient {
         req.toStream(writer);
         writer.flush();
         //trigger
-        new Message.ChatMessagePayload(MyUserIdentifier, "TEST 4_2 LISTEN TO INCOMING MESSAGES AND ECHO SPECIAL MESSAGE").toStream(writer);
-        writer.flush();
+        new Message.ChatMessagePayload(MyUserIdentifier, "TEST 5_1 LOST MESSAGE HANDLING").toStream(new DataOutputStream(bout));
+		DatagramPacket triggerMsg = new DatagramPacket(bout.toByteArray(),bout.toByteArray().length , InetAddress.getByName("130.75.202.197"),5005);
+		udpSocket.send(triggerMsg);
+		bout.reset();
+        
+        byte[] buffer = new byte[2000];
+        DatagramPacket recievedPacket = new DatagramPacket(buffer,buffer.length);
+        LinkedHashMap<String, byte[]> pendingList = new LinkedHashMap<>();
+
         
         
         
-        Message recieved_msg = null ;
-        while(true) {
-			try {
-				recieved_msg = Message.parse(reader);
-			} catch (IOException | ReflectiveOperationException e) {
-				e.printStackTrace();
-			}
-	    	if(  recieved_msg instanceof Message.ChatMessagePayload PayloadedMessage) {
-	    		String text =PayloadedMessage.getMessage();
-	    		if(text.contains("SPECIAL MESSAGE TEST 4_2")) {
-	    			recieved_msg.toStream(writer);
-	    			break;
-	    		}
-	    		
-	    	}
-	    }
+        
+        while (true) {
+            try {
+                udpSocket.receive(recievedPacket);
+            } catch (SocketTimeoutException e) {
+            	// if the socket does not receive any message until the timeout send the rest message
+	            for(String key  : pendingList.keySet()) {
+	            	
+	            	byte[] echoByte = pendingList.get(key);
+	            	DatagramPacket echoPacket = new DatagramPacket(echoByte, echoByte.length,InetAddress.getByName("130.75.202.197"), 5005);
+	            	udpSocket.send(echoPacket);
+	            	
+	            	
+	            }
+	            continue;    
+            } catch (IOException e) {
+                e.printStackTrace();
+                break; // exit loop on real I/O error
+            }
+
+            // process the packet 
+            ByteArrayInputStream bin = new ByteArrayInputStream(recievedPacket.getData(), 0, recievedPacket.getLength());
+            Message incoming = null;
+            try {
+                incoming = Message.parse(new DataInputStream(bin));
+            } catch (IOException | ReflectiveOperationException e) {
+                continue;
+            }
+            
+            if (incoming instanceof Message.ChatMessagePayload payloadedMessage) {
+                String text = payloadedMessage.getMessage();
+                System.out.println(text);
+                // remove Acknowledged message from our list
+                if (text.contains("Acknowledged message:")) {
+                	pendingList.remove(text);
+                    
+                }else if(text.contains("SUCCESSFULLY PASSED")) {// test succeeded
+                	
+                	break;
+                }else {// add  message to our list and send it
+                	
+                	// Save the **received bytes exactly as they came**
+                    byte[] receivedBytes = Arrays.copyOf( buffer, recievedPacket.getLength() );
+                    
+                    
+
+                    // Store in the map
+                    pendingList.put(text, receivedBytes);
+               }
+               
+            }
+            
+            if (pendingList.isEmpty() && testLanched) {
+                break;
+            }
+
+            
+        }
+
 	    writer.close();
 	    reader.close();
 	    socket.close();
+	    udpSocket.close();
 
     }
 }
